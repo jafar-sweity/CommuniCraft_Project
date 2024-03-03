cc2ed9fe2aa886eb755b7c3520f853f619d1a59c
ShahdKhader <shahdmahez@gmail.com>
2024-03-03 20:34:55 +0200

    files setup and first sequelize model(admin)

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-import express from 'express'
+// app.js
+import 'dotenv/config';
+import express from 'express';
+import sequelize from './config/sequelize.js';
+import router from './routes/router.js';
+import './models/Admins.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(router);
 
 
-const app=express()
-const PORT=3000;
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-}
-);
-
-app.listen(PORT,()=>{    
-    console.log(`Server is running on port ${PORT}`)
-}       
-);
-
-
-
+sequelize.sync().then(() => {
+  console.log('Database connected and models synced');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch(err => {
+  console.error('Failed to sync database models', err);
+});
