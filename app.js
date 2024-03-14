eabc514e39e4f314a4a6cd2da076dfac01a9e077
jafar-sweity <jata1518@gmail.com>
2024-03-14 17:23:17 +0200

    update docker-compose file

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ import express from 'express';
 import sequelize from './config/sequelize.js';
 import AuthRouter from './routes/AuthRouter.js';
 import cookieParser from 'cookie-parser';
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use('/auth', AuthRouter);
 app.get('/', (req, res) => {
-  res.send('Welcome tooooo the homepage');
+  res.send('Welcome too the homepage');
 });
 
 
